fe7d43fd20eec9ba985db337f0e4297085578a29
YEdraoui <y.edraoui@aui.ma>
2025-01-14 23:30:14 +0100

    New design, solve the css problem

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    // Add more if needed, e.g. "./public/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
